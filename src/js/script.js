3f142f24805c2b1373d1585baac7c50cfdb99b83
olivanova <ollia.ivanova@gmail.com>
2018-07-29 16:35:02 +0300

    added mark up index

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,35 @@
 svg4everybody(); // иницализация полифила для IE
 
 $(document).ready(function(){
-  // весь ваш код c jQuery
+  $("№carousel").slick({
+
+    // normal options...
+    infinite: false,
+
+    // the magic
+    responsive: [{
+
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          infinite: true
+        }
+
+      }, {
+
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          dots: true
+        }
+
+      }, {
+
+        breakpoint: 300,
+        settings: "unslick" // destroys slick
+
+      }]
+  });
 });
 
 // Если на проекте нет jQuery, но хочется $( document ).ready... (IE9+)
